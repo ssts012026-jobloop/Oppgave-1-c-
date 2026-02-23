d7d44a4be97f98be8f95828277ba2ac617d875d7
ssts012026 <ssts012026@gmail.com>
2026-02-23 19:47:26 +0100

    finito finale

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8c8f265aafcda169e4ce7edd4105bf5f77f4dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9e10fd455c7fe49a40fa61b6228042bc8e3cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
